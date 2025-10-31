cd2feee2a0c39bc82eb6a82141c38c3b0aca0b13
MariaE003 <elgotbymaria@gmail.com>
2025-10-31 09:56:00 +0100

    update case 1

--- js/script.js
+++ js/script.js
@@ -30,6 +30,7 @@ const erreurDFin = document.querySelector(".erreurDFin");
 const reservationInfo = JSON.parse(localStorage.getItem('reservations')) || [];
 // console.log(reservationInfo);
 AfficherCalendrier(reservationInfo);
+
 formulaire.addEventListener('submit', (e) => {
     e.preventDefault();
     validerFormulaire();
@@ -68,13 +69,7 @@ function validerFormulaire() {
                 erreurDDebut.innerHTML = "";
             }
         }
-        // else if(input.classList.contains('heureFin')){
-        //     erreurDFin.innerHTML="l'heur doit etre entre 12 et 23";
-        //     erreurDFin.style="color:red;";
-        //     input.value = "";
-        //     valide = false;
-        // }
-        //si condition debut superieur que date fin
+        //si condition heur debut superieur que heur fin
         else if (input.classList.contains('heureFin')) {
             if (parseInt(heureFin.value) < 12 || parseInt(heureFin.value > 23)) {
                 erreurDFin.innerHTML = "l'heur doit etre entre 12 et 23";
@@ -111,58 +106,90 @@ function validerFormulaire() {
         const nombrePersonnesValue = nombrePersonnes.value.trim();
         const typeRservationValue = typeRservation.value.trim();
         const jourReservationValue = jourReservation.value.trim();
-        const reservationObject = { id: Date.now(), Name: NameValue, heureDebut: heureDebutValue, heureFin: heureFinValue, nombrePersonnes: nombrePersonnesValue, typeRservation: typeRservationValue, jourReservation: jourReservationValue };
-        reservationInfo.push(reservationObject);
-        localStorage.setItem('reservations', JSON.stringify(reservationInfo));
+        const reservationObject = {
+            id: Date.now(), Name: NameValue, heureDebut: heureDebutValue, heureFin: heureFinValue,
+            nombrePersonnes: nombrePersonnesValue, typeRservation: typeRservationValue, jourReservation: jourReservationValue
+        };
+        
+        // let isunique = false;
+        // reservationInfo.forEach((e) => {
+        //     // if (parseInt(e.heureDebut) !== heureDebutValue || parseInt(e.heureFin) !== heureFinValue) {
+        //     if (parseInt(e.heureDebut) !== heureDebutValue ) {
+        //         isunique = true;
+        //     }
+        // })
+        // if (isunique) {
+        //     alert("ce temps est deja reserver choisir un autre temps");
+        //     formulaire.reset();
+        // } else {
+            reservationInfo.push(reservationObject);
+            addToLocalStorage(reservationInfo);
 
-        AfficherCalendrier(reservationInfo);
-        alert("Reservation enregistree avec succes !");
-        formulaire.reset();
-        countainerForm.style.display = 'none';
+            AfficherCalendrier(reservationInfo);
+            alert("Reservation enregistree avec succes !");
+            formulaire.reset();
+            countainerForm.style.display = 'none';
+        // }
+
 
     }
 }
 
+function addToLocalStorage(reservationInfo) {
+    localStorage.setItem('reservations', JSON.stringify(reservationInfo));
 
+}
 
 function AfficherCalendrier(reservationInfo) {
-    const case1Div = document.querySelector(".case1");
 
-    console.log(reservationInfo[0].jourReservation);
+    const case1Div = document.querySelector(".case1");
+    const case2Div = document.querySelector(".case2");
+    const case3Div = document.querySelector(".case3");
+    const case4Div = document.querySelector(".case4");
+    const case5Div = document.querySelector(".case5");
+
+    case1Div.innerHTML = "";
+    case2Div.innerHTML = "";
+    case3Div.innerHTML = "";
+    case4Div.innerHTML = "";
+    case5Div.innerHTML = "";
+
+    // console.log(reservationInfo[0].jourReservation);
 
     // res.innerHTML=reservationInfo[0].Name;
     // console.log(res);
 
     reservationInfo.forEach((e) => {
 
-        switch (parseInt(reservationInfo[0].jourReservation)) {
+        switch (parseInt(e.jourReservation)) {
             case 1:
-                const res = document.createElement("div");
-                res.innerHTML = `${e.Name}
+                const div1 = document.createElement("div");
+                div1.innerHTML = `${e.Name}
                 ${e.heureDebut}
                 ${e.heureFin}
                 ${e.nombrePersonnes}
+                 <div class="div-btn-reservation">
+                <button class="edit-btn" id="${e.id}"><img src="images/edit.svg"alt="none"></button>
+                <button class="delete-btn" id="${e.id}"><img src="images/delete.svg"alt="none"></button>
+                </div>
                 `;
-                case1Div.appendChild(res);
-                res.style = "display: flex;font-size: 14px; align-items: center; width: 93px; height: 7em; background: #c79ddc; text-align: center; border-radius: 3px; border-left: 4px #1c023f5c solid; ";
+                case1Div.appendChild(div1);
+                virifierTime(e.heureDebut, e.heureFin, div1);
                 // console.log(case1Div);
                 break;
             case 2:
+               
                 break;
             case 3:
+                
+
                 break;
             case 4:
-                break;
+               
             case 5:
-                break;
-            case 6:
-                break;
-            default:
-                break;
+                
         }
-
         
-
 
     })
 }
